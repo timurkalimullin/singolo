4f9df2cda8b34641bc11c452b6df05044ebd5223
Timur Kalimullin <timur_kalimullin@list.ru>
2020-03-14 04:43:12 +0500

    feat: add slider and toggle screens

--- script.js
+++ script.js
@@ -53,46 +53,126 @@ const operations = {
     },
 
     modal: ()=> {
-        const submit = document.getElementById('formSubmit'),
+        const submit = document.getElementById('formSingolo'),
         subject = document.getElementById('subject_input'),
         descr = document.getElementById('project_description'),
         name = document.getElementById('name_input'),
         email = document.getElementById('email_input');
 
-        submit.addEventListener('click', (e)=>{
+        submit.addEventListener('submit', (e)=>{
             e.preventDefault();
 
-            if (!/^[a-z0-9_-]{2,16}$/.test(name.value) || !/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(email.value)) {
-                alert('Введите корректные контактные данные');
+            if (!/^[a-z0-9_-]{2,16}$/.test(name.value)) {
+                alert('Введите корректное имя, от 2 до 16 символов');
+                return;
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(email.value)) {
+                alert('Введите корректную электронную почту');
                 return;
             }
 
-            if (subject.value.toLowerCase() == 'singolo') {
-                document.getElementById('modalSubj').innerHTML = 'Тема: Singolo';
-            } else {document.getElementById('modalSubj').innerHTML = 'Без темы';}
-            if (descr.value.toLowerCase() == 'portfolio project') {
-                document.getElementById('modalDescr').innerHTML = 'Описание: Portfolio project';
-            } else {document.getElementById('modalDescr').innerHTML = 'Без описания'}
+            if (subject.value) {
+                document.getElementById('modalSubj').innerHTML = `Тема: ${subject.value}`;
+            } else if (subject.value.toLowerCase() == '') {
+                document.getElementById('modalSubj').innerHTML = 'Без темы'
+            }
+            
+            if (descr.value) {
+                document.getElementById('modalDescr').innerHTML = `Описание: ${descr.value}`;
+            } else if (descr.value.toLowerCase() == '') {
+                document.getElementById('modalDescr').innerHTML = 'Без описания'
+            }
+            
 
             document.getElementById('modal').style.display = 'block';
+            document.body.style.overflow = "hidden";
         })
 
         document.getElementById('modalBtn').addEventListener('click', (e)=>{
             e.preventDefault();
             document.getElementById('modalSubj').innerHTML = '';
             document.getElementById('modalDescr').innerHTML = '';
+            document.body.style.overflow = "";
             document.getElementById('formSingolo').reset();
             document.getElementById('modal').style.display = 'none';
 
         })
-    }
+    },
+
     
+    slider:()=>{
+        let slides = document.querySelectorAll('.slide'),
+        activeSlide = 0,
+        isEnabled = true;
+
+        function changeActiveSlide(n) {
+            activeSlide = (n + slides.length)%slides.length;
+        }
+
+        function previousSlide(n) {
+            hideSlide('to-right')
+            changeActiveSlide(n-1);
+            showSlide('from-left');
+        }
+
+        function nextSlide(n) {
+            hideSlide('to-left')
+            changeActiveSlide(n+1);
+            showSlide('from-right');
+        }
+
+        function hideSlide(direction) {
+            isEnabled = false;
+            slides[activeSlide].classList.add(direction);
+            slides[activeSlide].addEventListener('animationend', function(){
+                this.classList.remove('active', direction);
+            })
+        }
+
+        function showSlide(direction) {
+            slides[activeSlide].classList.add('coming', direction);
+            slides[activeSlide].addEventListener('animationend', function(){
+                this.classList.remove('coming', direction);
+                this.classList.add('active');
+                isEnabled = true;
+            })
+        }
+
+        function changeColor() {
+            document.querySelector('.slider').classList.toggle('blue');
+        }
+
+        document.querySelector('#left').addEventListener('click', function() {
+        if (isEnabled) {
+            previousSlide(activeSlide);
+            changeColor();
+        }
+        })
+
+        document.querySelector('#right').addEventListener('click', function() {
+        if (isEnabled) {
+            nextSlide(activeSlide);
+            changeColor();
+        }
+        })
+
+    },
+    
+    screenToggle:()=>{
+        let phone = document.querySelectorAll('.phone_body');
+        phone.forEach((el,i)=>{
+            el.addEventListener('click', function(event){
+                console.log(this)
+                event.target.parentElement.querySelector('.screen').classList.toggle('hide')
+            })
+        })
+    }
 }
 
 operations.anchors();
 operations.portfolio();
 operations.modal();
-
+operations.slider();
+operations.screenToggle();
 
 
 
